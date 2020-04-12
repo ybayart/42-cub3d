e35559914833cc2228a416f283cb8a9457908c2c
hexablack <hexaminecraft@gmail.com>
2020-04-12 05:52:47 +0200

    improve fix mlx

--- linux/minilibx/mlx_loop.c
+++ linux/minilibx/mlx_loop.c
@@ -16,29 +16,34 @@ extern int	(*(mlx_int_param_event[]))();
 
 int		mlx_loop(t_xvar *xvar)
 {
-  XEvent	ev;
-  t_win_list	*win;
-  Atom wm_delete_window;
+	XEvent		ev;
+	t_win_list	*win;
+	Atom		wm_delete_window;
 
-  wm_delete_window = XInternAtom (xvar->display, "WM_DELETE_WINDOW", False);
-  mlx_int_set_win_event_mask(xvar);
-  xvar->do_flush = 0;
-  XSetWMProtocols (xvar->display, xvar->win_list->window, &wm_delete_window, 1);
-  while (42)
-    {
-      while (!xvar->loop_hook || XPending(xvar->display))
+	wm_delete_window = XInternAtom (xvar->display, "WM_DELETE_WINDOW", False);
+	mlx_int_set_win_event_mask(xvar);
+	xvar->do_flush = 0;
+	win = xvar->win_list;
+	while (win)
 	{
-	  XNextEvent(xvar->display,&ev);
-	  win = xvar->win_list;
-	  while (win && (win->window!=ev.xany.window))
-	    win = win->next;
-	  if (ev.type == ClientMessage) {
-        if ((Atom)ev.xclient.data.l[0] == wm_delete_window)
-	      XDestroyWindow(xvar->display, xvar->win_list->window);
-	  if (win && ev.type < MLX_MAX_EVENT)
-	    if (win->hooks[ev.type].hook)
-	      mlx_int_param_event[ev.type](xvar, &ev, win);
+		XSetWMProtocols (xvar->display, win->window, &wm_delete_window, 1);
+		win = win->next;
 	}
-      xvar->loop_hook(xvar->loop_param);
-    }
+	while (42)
+	{
+		while (!xvar->loop_hook || XPending(xvar->display))
+		{
+			XNextEvent(xvar->display,&ev);
+			win = xvar->win_list;
+			while (win && (win->window!=ev.xany.window))
+				win = win->next;
+			if (ev.type == ClientMessage)
+				if ((Atom)ev.xclient.data.l[0] == wm_delete_window)
+					XDestroyWindow(xvar->display, win->window);
+			if (win && ev.type < MLX_MAX_EVENT)
+				if (win->hooks[ev.type].hook)
+					mlx_int_param_event[ev.type](xvar, &ev, win);
+		}
+		xvar->loop_hook(xvar->loop_param);
+	}
 }
